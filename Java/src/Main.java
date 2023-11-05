d96b50db58e6cf8b64001bb090351d2665e581c1
BusisiweMakhubu <128509992+BusisiweMakhubu@users.noreply.github.com>
2023-11-05 23:43:09 +0200

    Add files via upload

---
+++ Java/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6};
+        int result = SumEvenNumbers.sumEvenNumbers(numbers);
+        System.out.println(result);
+    }
+}
+
